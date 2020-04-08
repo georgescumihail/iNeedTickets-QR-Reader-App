f92cbc518c3c4337d66437dbdb2353b6fcd36e25
Mihail Georgescu <geo.mihail96@gmail.com>
2020-04-09 01:20:51 +0300

    Implemented basic QR reading and displaying using the zxing library

---
+++ app/src/main/java/ro/ineedtickets/reader/MainActivity.java
@@ -0,0 +1,55 @@
+package ro.ineedtickets.reader;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button scanButton;
+
+    private String ticketId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scanButton = findViewById(R.id.scan_button);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setOrientationLocked(false);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result != null){
+            if(result.getContents() != null){
+                ticketId = result.getContents();
+                Toast.makeText(this, ticketId, Toast.LENGTH_LONG).show();
+            }
+        }
+        else{
+            Toast.makeText(this, "There was an error in retrieving the data", Toast.LENGTH_LONG).show();
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
